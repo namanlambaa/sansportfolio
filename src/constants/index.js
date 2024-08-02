0efe9195239b99c0cffa189144ccb7fe4aa4a5dc
Naman Lamba <rocklean17102001@gmail.com>
2024-08-03 01:06:50 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -45,11 +45,15 @@ export const navLinks = [
 
 const services = [
   {
-    title: "Web Developer",
+    title: "Marketing",
     icon: web,
   },
   {
-    title: "Software Developer",
+    title: "Management",
+    icon: backend,
+  },
+  {
+    title: "Economics",
     icon: backend,
   },
   /*
@@ -69,26 +73,26 @@ const services = [
 ];
 
 const technologies = [
-  {
-    name: "HTML 5",
-    icon: html,
-  },
-  {
-    name: "CSS 3",
-    icon: css,
-  },
-  {
-    name: "JavaScript",
-    icon: javascript,
-  },
+  // {
+  //   name: "HTML 5",
+  //   icon: html,
+  // },
+  // {
+  //   name: "CSS 3",
+  //   icon: css,
+  // },
+  // {
+  //   name: "JavaScript",
+  //   icon: javascript,
+  // },
   // {
   //   name: "TypeScript",
   //   icon: typescript,
   // },
   {
-    name: "React JS",
-    icon: reactjs,
-  },
+  //   name: "React JS",
+  //   icon: reactjs,
+  // },
   // {
   //   name: "Python",
   //   icon: python,
@@ -97,22 +101,22 @@ const technologies = [
   //   name: "Tailwind CSS",
   //   icon: tailwind,
   // },
-  {
-    name: "Node JS",
-    icon: nodejs,
-  },
-  {
-    name: "MongoDB",
-    icon: mongodb,
-  },
+  // {
+  //   name: "Node JS",
+  //   icon: nodejs,
+  // },
+  // {
+  //   name: "MongoDB",
+  //   icon: mongodb,
+  // },
   // {
   //   name: "Three JS",
   //   icon: threejs,
   // },
-  {
-    name: "git",
-    icon: git,
-  },
+  // {
+  //   name: "git",
+  //   icon: git,
+  // },
   // {
   //   name: "figma",
   //   icon: figma,
@@ -125,74 +129,84 @@ const technologies = [
 
 const experiences = [
   {
-    title: "Software Development Intern",
-    company_name: "TechBrig.io",
+    title: "Junior Marketing Manager",
+    company_name: "Delhi Public School, 67A",
     icon: ongc,
     iconBg: "#E6DEDD",
-    date: "July 2023 - September 2023",
+    date: "June 2023 - Present",
     points: [
-      "Enhanced the official student and faculty portal for IIM Indore, serving over 2000 users and boosting accessibility by 30%",
-      "Created features like a student marketplace, timetable management, and campus-wide connectivity, using NextJS,MongoDB, and Redis, increasing student engagement by 25%.",
-      "Helped design HRM (Human Resource Management) solutions for two global clients such as Tata and Birla,featuring functionalities like employee leaves, pay slips, team progress tracking, and task management.",
-      "Improved features, user interfaces and application performance, reducing load times by 15%.",
-      "•	Worked collaboratively with the team, incorporating feedback through code reviews, leading to enhanced code quality and increasing team productivity by 20%.",
+      "Executed impact full social media campaigns increasing brand awareness by 646.43%",
+      "Conducted outreach and lead generation activities, building a pipeline of new potential clients through targeted marketing campaigns",
+      "Lead a team of 7 members to create and implement organisation specific marketing initiatives that corresponds with the organisation's identity and goals.",
+      "Managed a marketing campaign budget, ensuring all branding activities stayed within budget and delivered maximum ROI.",
+      "Managed the website and all social media accounts, ensuring they were consistently up-to-date.",
+      "Restructured communication flow among all the departments and cut down paperwork by 75%.",
+      "Conducted workload assessments and devised new operational processes that led to a 40% increase in productivity",
     ],
   },
 
   
 
   {
-    title: "Web Development Intern",
-    company_name: "LazerIndia",
+    title: "Social Media and Marketing Intern",
+    company_name: "Aquarain Immigration Services",
     icon: starbucks,
     iconBg: "#383E56",
-    date: "February 2024 - May 2024",
+    date: "April 2024 - Present",
     points: [
-      "Led a team of 5 interns to design and develop a sales auditing website.",
-      "Elevated Lazer India's website, resulting in a 40% increase in user engagement and a 25% reduction in bounce rate",
-      "Achieved a remarkable 40% improvement in sales data accuracy through analysis and innovative solutions.",
-      "Coordinated seamlessly with team members to meet all project deadlines and objectives within one month.",
+      "Executed impact full social media campaigns increasing brand awareness by 1,974%",
+      "Conducted market research, developed presentations and created engagement plans.",
+      "Managed the website and all social media profiles, ensuring that they were always up to date.",
+      "Created engaging content, including blogs and articles, and provided copywriting for all social media profiles.",
+      "Supported duties such as meeting scheduling and marketing database management.",
+      "Kept abreast of market developments and best practices in order to develop abilities and make valuable contributions to the team.",
+      "Was recognized as the intern of the month in June 2024.",
     ],
   },
-  // {
-  //   title: "React Native Developer",
-  //   company_name: "Tesla",
-  //   icon: tesla,
-  //   iconBg: "#E6DEDD",
-  //   date: "Jan 2021 - Feb 2022",
-  //   points: [
-  //     "Developing and maintaining web applications using React.js and other related technologies.",
-  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-  //     "Implementing responsive design and ensuring cross-browser compatibility.",
-  //     "Participating in code reviews and providing constructive feedback to other developers.",
-  //   ],
-  // },
-  // {
-  //   title: "Web Developer",
-  //   company_name: "Shopify",
-  //   icon: shopify,
-  //   iconBg: "#383E56",
-  //   date: "Jan 2022 - Jan 2023",
-  //   points: [
-  //     "Developing and maintaining web applications using React.js and other related technologies.",
-  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-  //     "Implementing responsive design and ensuring cross-browser compatibility.",
-  //     "Participating in code reviews and providing constructive feedback to other developers.",
-  //   ],
-  // },
-  // {
-  //   title: "Full stack Developer",
-  //   company_name: "Meta",
-  //   icon: meta,
-  //   iconBg: "#E6DEDD",
-  //   date: "Jan 2023 - Present",
-  //   points: [
-  //     "Developing and maintaining web applications using React.js and other related technologies.",
-  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-  //     "Implementing responsive design and ensuring cross-browser compatibility.",
-  //     "Participating in code reviews and providing constructive feedback to other developers.",
-  //   ],
-  // },
+  {
+    title: "Sales and Marketing Intern",
+    company_name: "ILP Overseas",
+    icon: tesla,
+    iconBg: "#E6DEDD",
+    date: "Jan 2022 - Feb 2022",
+    points: [
+      "Assisted the sales team in discovering and qualifying leads through research and outreach efforts.",
+      "Helped to prepare sales presentations and proposals for potential clients.",
+      "Maintaining and updating customer relationship management (CRM) systems to track sales activities and client interactions.",
+      "Conducted market research to find new business prospects and trends.",
+      "Participated in sales meetings and training courses to improve product knowledge and sales skills.",
+    ],
+  },
+  {
+    title: "Finance Intern",
+    company_name: "NGO Sakaar Outreach",
+    icon: shopify,
+    iconBg: "#383E56",
+    date: "Jan 2022 - Feb 2022",
+    points: [
+      "Carried out market research to assist with financial planning and investment plans.",
+      "Collaborated with senior finance team members to evaluate financial performance and identify opportunities for improvement.",
+      "Helped maintain financial records and documentation in accordance with company policy and regulatory standards.",
+      "Participated in team meetings and contributed to discussions on financial strategy and initiatives.",
+    ],
+  },
+  {
+    title: "Marketing Intern",
+    company_name: "Delhi Public School, 67A",
+    icon: meta,
+    iconBg: "#E6DEDD",
+    date: "June 2021 - Dec 2021",
+    points: [
+      "Conducted market research to uncover trends and customer preferences to aid sales strategy.",
+      "Assisted in the creation of marketing materials such as blog posts, social media content, and email campaigns.",
+      "Managed and updated the company's social media profiles, interacted with followers, and analysed performance data.",
+      "Supported the sales team by creating presentations, updating customer databases, and assisting with lead development.",
+      "Helped to organise and promote events and trade exhibits.",
+      "Interacted with customers through phone and email to get their opinions and feedback with questions.",
+      "Tracked and reported on marketing campaign performance",
+      "Collaborated with cross-functional teams to ensure goals and objectives were aligned.",
+    ],
+  },
 
   
 ];
@@ -228,116 +242,157 @@ const testimonials = [
 
 const projects = [
   {
-    name: "SyncIT",
+    name: "University of Michigan",
     description:
-      "Built a cross-device communication platform that allows for seamless transfer of data between connected devices, using a custom TCP-based protocol for fast, efficient and secure data transfer utilizing Protocol Buffers.",
+      "Completed course on Successful Negotiation- Essential Strategies and Skills",
     tags: [
-      {
-        name: "FLutter",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "ElectronJS",
-        color: "green-text-gradient",
-      },
-      {
-        name: "NodeJS",
-        color: "pink-text-gradient",
-      },
+      // {
+      //   name: "Negotiation",
+      //   color: "blue-text-gradient",
+      // },
+      // {
+      //   name: "Marketing",
+      //   color: "green-text-gradient",
+      // },
+      // {
+      //   name: "NodeJS",
+      //   color: "pink-text-gradient",
+      // },
     ],
     image: crmext,
-    source_code_link: "https://github.com/namandureja/glue-all",
+    // source_code_link: "https://github.com/namandureja/glue-all",
   },
   {
-    name: "WeatherX",
+    name: "University of Amsterdam",
     description:
-      "Engineered and deployed WeatherX, a weather information website offering real-time updates on three main factors: temperature, rain chances, pressure, and more.",
+      "Completed course on Quantitative method",
     tags: [
-      {
-        name: "HTML",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "CSS",
-        color: "green-text-gradient",
-      },
-      {
-        name: "JavaScript",
-        color: "pink-text-gradient",
-      },
+      // {
+      //   name: "HTML",
+      //   color: "blue-text-gradient",
+      // },
+      // {
+      //   name: "CSS",
+      //   color: "green-text-gradient",
+      // },
+      // {
+      //   name: "JavaScript",
+      //   color: "pink-text-gradient",
+      // },
     ],
     image: gemini, // Replace with the actual path to the image
-    source_code_link: "https://namanlambaa.github.io/WeatherX/", // Replace with the actual GitHub link
+    // source_code_link: "https://namanlambaa.github.io/WeatherX/", // Replace with the actual GitHub link
   },
 
-  /*
+  
 
   {
-    name: "Car Rent",
+    name: "Duke University",
     description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      "Completed course on Data Science Math Skills",
     tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "mongodb",
-        color: "green-text-gradient",
-      },
-      {
-        name: "tailwind",
-        color: "pink-text-gradient",
-      },
+      // {
+      //   name: "react",
+      //   color: "blue-text-gradient",
+      // },
+      // {
+      //   name: "mongodb",
+      //   color: "green-text-gradient",
+      // },
+      // {
+      //   name: "tailwind",
+      //   color: "pink-text-gradient",
+      // },
     ],
     image: carrent,
-    source_code_link: "https://github.com/",
+    // source_code_link: "https://github.com/",
   },
 
   
   {
-    name: "Job IT",
+    name: "University of Michigan",
     description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      "Completed course on Beyond the Sustainable Development Goals (SDGs)- Addressing Sustainability and Development",
     tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "restapi",
-        color: "green-text-gradient",
-      },
-      {
-        name: "scss",
-        color: "pink-text-gradient",
-      },
+      // {
+      //   name: "react",
+      //   color: "blue-text-gradient",
+      // },
+      // {
+      //   name: "restapi",
+      //   color: "green-text-gradient",
+      // },
+      // {
+      //   name: "scss",
+      //   color: "pink-text-gradient",
+      // },
     ],
     image: jobit,
-    source_code_link: "https://github.com/",
+    // source_code_link: "https://github.com/",
   },
   {
-    name: "Trip Guide",
+    name: "Erasmus University Rotterdam",
     description:
-      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      "Completed course on Earth economics",
     tags: [
-      {
-        name: "nextjs",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "supabase",
-        color: "green-text-gradient",
-      },
-      {
-        name: "css",
-        color: "pink-text-gradient",
-      },
+      // {
+      //   name: "nextjs",
+      //   color: "blue-text-gradient",
+      // },
+      // {
+      //   name: "supabase",
+      //   color: "green-text-gradient",
+      // },
+      // {
+      //   name: "css",
+      //   color: "pink-text-gradient",
+      // },
     ],
     image: tripguide,
-    source_code_link: "https://github.com/",
+    // source_code_link: "https://github.com/",
   },
-  */
+  {
+    name: "University of Illinois at Urbana-Champaign",
+    description:
+      "Completed course on Country Level Economics- Macroeconomic Variables and Markets",
+    tags: [
+      // {
+      //   name: "nextjs",
+      //   color: "blue-text-gradient",
+      // },
+      // {
+      //   name: "supabase",
+      //   color: "green-text-gradient",
+      // },
+      // {
+      //   name: "css",
+      //   color: "pink-text-gradient",
+      // },
+    ],
+    image: tripguide,
+    // source_code_link: "https://github.com/",
+  },
+  {
+    name: "Duke University",
+    description:
+      "Competed course on Impact Measurement & Management for the SDGs",
+    tags: [
+      // {
+      //   name: "nextjs",
+      //   color: "blue-text-gradient",
+      // },
+      // {
+      //   name: "supabase",
+      //   color: "green-text-gradient",
+      // },
+      // {
+      //   name: "css",
+      //   color: "pink-text-gradient",
+      // },
+    ],
+    image: tripguide,
+    // source_code_link: "https://github.com/",
+  },
 ];
 
 export { services, technologies, experiences, testimonials, projects };
