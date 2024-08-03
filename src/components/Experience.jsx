b3bc522399e5a61e525b765e4f3f0397ad932cc3
Naman Lamba <rocklean17102001@gmail.com>
2024-08-04 02:08:42 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -55,7 +55,7 @@ const Experience = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}> What have i done so far!</p>
+        <p className={styles.sectionSubText}>What have i done so far!</p>
         <h2 className={styles.sectionHeadText}>Work Experience</h2>
       </motion.div>
 
